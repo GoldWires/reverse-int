028962d91890c36a929f2cc54bec407211025311
GoldWires <67865290+GoldWires@users.noreply.github.com>
2020-10-24 16:12:52 +0300

    init: make function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+      n = n*(-1);
+  }
+  var p = '';
+  for(var i = n.toString().length - 1; i >= 0; i--){
+    p = p.toString() + n.toString()[i];
+  }
+  return p;
 }
